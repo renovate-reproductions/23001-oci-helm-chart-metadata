f3fccfeceb4fa84a903917db4d22ae79ff6bdae4
Michael Kriese <michael.kriese@visualon.de>
2023-08-30 10:29:36 +0200

    chore: downgrade chart

--- test.tf
+++ test.tf
@@ -2,5 +2,5 @@ resource "helm_release" "kube-prometheus" {
   name       = "kube-prometheus"
   repository = "oci://registry-1.docker.io/bitnamicharts"
   chart      = "kube-prometheus"
-  version    = "8.17.2"
+  version    = "8.17.1"
 }
